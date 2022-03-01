0cd662e8222db8e81e59343df36328e6d31c61ea
prinnweth <100203469+prinnweth@users.noreply.github.com>
2022-03-01 18:29:35 +0700

    Add files via upload

---
+++ Notifier.py
@@ -0,0 +1,4 @@
+from pyler import notification
+title = 'Hello Amazing people!'
+message= 'Thankyou for reading! Take care!'
+notification.notify(title= title,message= message,app_icon = None,timeout= 10,toast=False)
